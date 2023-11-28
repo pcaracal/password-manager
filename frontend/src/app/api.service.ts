97bcbae923b2060b13de1ce7d9dbd009dfdb3e6f
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-11-28 20:16:20 +0100

    feat: folders exist

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -41,25 +41,41 @@ export class ApiService {
     return this._http.get(this._url + '/data', httpOptions);
   }
 
-  dataPost(name: string, username: string, password: string, url: string, notes: string): Observable<any> {
+  dataPost(name: string, username: string, password: string, url: string, notes: string, fk_folder_id: number): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Authorization': sessionStorage.getItem('token') || ''
       })
     }
 
-    const body = {fk_user_id: -1, name: name, username: username, password: password, url: url, notes: notes};
+    const body = {
+      fk_user_id: -1,
+      name: name,
+      username: username,
+      password: password,
+      url: url,
+      notes: notes,
+      fk_folder_id: fk_folder_id
+    };
     return this._http.post(this._url + '/data', body, httpOptions);
   }
 
-  dataPatch(id: number, name: string, username: string, password: string, url: string, notes: string): Observable<any> {
+  dataPatch(id: number, name: string, username: string, password: string, url: string, notes: string, fk_folder_id: number): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Authorization': sessionStorage.getItem('token') || ''
       })
     }
 
-    const body = {fk_user_id: -1, name: name, username: username, password: password, url: url, notes: notes};
+    const body = {
+      fk_user_id: -1,
+      name: name,
+      username: username,
+      password: password,
+      url: url,
+      notes: notes,
+      fk_folder_id: fk_folder_id
+    };
     return this._http.patch(this._url + '/data/' + id, body, httpOptions);
   }
 
@@ -82,5 +98,47 @@ export class ApiService {
 
     const body = {password: password};
     return this._http.patch(this._url + '/user', body, httpOptions);
+  }
+
+  folderPost(newFolderName: string) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': sessionStorage.getItem('token') || ''
+      })
+    }
+
+    const body = {name: newFolderName, fk_user_id: -1};
+    return this._http.post(this._url + '/folder', body, httpOptions);
+  }
+
+  getFolders(): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': sessionStorage.getItem('token') || ''
+      })
+    }
+
+    return this._http.get(this._url + '/folder', httpOptions);
+  }
+
+  deleteFolder(id: number): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': sessionStorage.getItem('token') || ''
+      })
+    }
+
+    return this._http.delete(this._url + '/folder/' + id, httpOptions);
+  }
+
+  folderPatch(id: number, name: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': sessionStorage.getItem('token') || ''
+      })
+    }
+
+    const body = {name: name};
+    return this._http.patch(this._url + '/folder/' + id, body, httpOptions);
   }
 }
