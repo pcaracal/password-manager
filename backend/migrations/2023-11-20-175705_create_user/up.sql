2b83df945c188af90717f10b75635ae0df8490dd
pcaracal <peter.schreivogel@stud.kbw.ch>
2023-11-28 07:43:35 +0100

    fix: remove dev data from sql migrations

--- backend/migrations/2023-11-20-175705_create_user/up.sql
+++ backend/migrations/2023-11-20-175705_create_user/up.sql
@@ -5,6 +5,3 @@ create table user
     username text not null,
     password text not null
 );
-
-insert into user (username, password)
-values ('admin', '$argon2id$v=19$m=19456,t=2,p=1$4rOtUTyQiq6PUlOvMx7V9g$dCrATPxx++u8B9FAuQEOe4ZdT1M1xXt9fnFoap1IXKA');
--- backend/migrations/2023-11-20-180115_create_data/up.sql
+++ backend/migrations/2023-11-20-180115_create_data/up.sql
@@ -12,13 +12,3 @@ create table data
     notes      text,
     foreign key (fk_user_id) references user (id)
 );
-
-insert into data (fk_user_id, name, username, password, created_at, updated_at, url, notes)
-values (1,
-        'U2FsdGVkX19pgltZJtyDm1mIUjpB6guWENTcWZxz0JvLVAgz3ED7MPdh3CgUDrN2fiqNGej8cYRKO9maZ+lrC3pcJNeHwTZdCvwpdx1M609xNWccScd8DYzSx9O+LK+yokRnXLFniv87ryc9K2/eF1ecZsCpSWnfm+bo/LZwWSmJ6UauTES1yhKQ5yNRdWnsy3WySdGamrBfqdINb69/Qg==',
-        'U2FsdGVkX19pgltZJtyDm1mIUjpB6guWENTcWZxz0JvLVAgz3ED7MPdh3CgUDrN2fiqNGej8cYRKO9maZ+lrC3pcJNeHwTZdCvwpdx1M609xNWccScd8DYzSx9O+LK+yokRnXLFniv87ryc9K2/eF1ecZsCpSWnfm+bo/LZwWSmJ6UauTES1yhKQ5yNRdWnsy3WySdGamrBfqdINb69/Qg==',
-        'U2FsdGVkX19pgltZJtyDm1mIUjpB6guWENTcWZxz0JvLVAgz3ED7MPdh3CgUDrN2fiqNGej8cYRKO9maZ+lrC3pcJNeHwTZdCvwpdx1M609xNWccScd8DYzSx9O+LK+yokRnXLFniv87ryc9K2/eF1ecZsCpSWnfm+bo/LZwWSmJ6UauTES1yhKQ5yNRdWnsy3WySdGamrBfqdINb69/Qg==',
-        1701087524000,
-        1701087524000,
-        'U2FsdGVkX19pgltZJtyDm1mIUjpB6guWENTcWZxz0JvLVAgz3ED7MPdh3CgUDrN2fiqNGej8cYRKO9maZ+lrC3pcJNeHwTZdCvwpdx1M609xNWccScd8DYzSx9O+LK+yokRnXLFniv87ryc9K2/eF1ecZsCpSWnfm+bo/LZwWSmJ6UauTES1yhKQ5yNRdWnsy3WySdGamrBfqdINb69/Qg==',
-        'U2FsdGVkX19pgltZJtyDm1mIUjpB6guWENTcWZxz0JvLVAgz3ED7MPdh3CgUDrN2fiqNGej8cYRKO9maZ+lrC3pcJNeHwTZdCvwpdx1M609xNWccScd8DYzSx9O+LK+yokRnXLFniv87ryc9K2/eF1ecZsCpSWnfm+bo/LZwWSmJ6UauTES1yhKQ5yNRdWnsy3WySdGamrBfqdINb69/Qg==');
